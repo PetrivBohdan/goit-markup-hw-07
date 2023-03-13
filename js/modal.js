eec706b531c092ef04c1081d3b6ac2c54c29fa93
PetrivBohdan <blancdols@gmail.com>
2023-03-13 15:16:20 +0200

    update

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -12,20 +13,3 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-
-})();
-
